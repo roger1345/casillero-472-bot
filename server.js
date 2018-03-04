1eb02c8ffcfb0f73a420bb5f70d9b31232e04560
Roger <roger1345@gmail.com>
2018-03-04 15:09:09 -0300

    using api bot

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function getTrackingInfo(ctx){
 
 bot.command('consultar', (ctx) => {
   getTrackingInfo(ctx).then(function(result) {
-    bot.telegram.sendMessage(ctx.update.message.chat.id, result, Extra.markdown.toString().toLowerCase());
+    bot.telegram.sendMessage(ctx.update.message.chat.id, result, 'markdown');
     }, function(err) {
       console.log(err);
     });
