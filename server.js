caed11314196737f771ebb1d2a986e4b846d252f
Roger <roger1345@gmail.com>
2018-03-03 23:39:04 -0300

    using api bot

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ function getTrackingInfo(ctx){
 bot.command('consultar', (ctx) => {
   getTrackingInfo(ctx).then(function(result) {
     console.log("exito");  
-    return ctx.reply(result);
+    ctx.reply(result);
     }, function(err) {
       console.log(err);
     });
