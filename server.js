70ddc7b988ac4f2dfc387eb3ed68fe451b49fb34
Roger <roger1345@gmail.com>
2018-03-03 22:09:06 -0300

    using api bot

--- server.js
+++ server.js
@@ -3,157 +3,48 @@ var express = require('express'),
     app     = express(),
     Telegraf = require('telegraf');
 
-const { reply } = Telegraf
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-console.log(process.env.TELEGRAM_BOT_TOKEN);
-
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
-
-/*app.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
-});*/
-
 app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized
-    res.send('{ pageCount: -1 }');
-
+  res.send('pagecounting');
 });
 
-var sendMessage= function(req,res,text){
-    var data = {
-        'chat_id' : req.body.message.chat.id,
-        'text': text,
-        'parse_mode': 'Markdown'
-    };
-    console.log(data);
-    var request = require('request');
-    var options = {
-      uri: 'https://api.telegram.org/bot525279818:AAFlIkZEzWHhuuBYJZ96_YR7y6imZYhEYzQ/sendMessage',
-      method: 'POST',
-      json: data,
-      headers: {
-         'Content-Type': 'application/json'
-      }
-    };
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-        res.status(200).end();
+var getTrackingInfo= function(req,res,text){
+  let message="";
+  let parametro=req.body.message.text;
+  parametro=parametro.replace("/consultar ","");
+
+  var request = require('request');
+  var options = {
+    uri: 'https://casillerovirtual4-72.com.co/Registration/Tracking/gettrackingresult?trackinno='+parametro,
+    method: 'GET',
+    headers: {
+       'Content-Type': 'application/json'
+    }
+  };
+  console.log(options);
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      let jsonTracking=JSON.parse(body);
+      if(!isEmpty(jsonTracking)){
+        message="Hola *"+req.body.message.from.first_name+"*, su numero de guia *"+parametro+"* tiene estado *"+jsonTracking.dessta+"* enviado por *"+jsonTracking.rem_nombre+"*. Tubo un peso de *"+jsonTracking.pesolb+"* lbs, se recibio el dia *"+jsonTracking.recibo+"* y se recibio el pago el dia *"+jsonTracking.pagado+"*.";
       }else{
-        console.log(error);
+        message="No se encontro informacion sobre la guia *"+parametro+"*, valide su guia.";
       }
-    });
+    }
+  });
+  return message;
 };
-bot.command('consultar', (ctx) => ctx.replyWithPhoto({ url: 'https://picsum.photos/200/300/?random' }))
+
+bot.command('consultar', (ctx) => ctx.sendMessage(ctx.message.chat.id,getTrackingInfo()))
 bot.on('text', ({ replyWithHTML }) => replyWithHTML('<b>Hey there!</b>'))
 app.use(bot.webhookCallback('/'))
-
-/*app.post('/', function(req,res){
-
-if (typeof req.body !== 'undefined' && typeof req.body.message !== 'undefined' && typeof req.body.message.text !== 'undefined' && req.body.message.text ){
-
-if(req.body.message.text.startsWith('/consultar')){
-
-    let parametro=req.body.message.text;
-    parametro=parametro.replace("/consultar ","");
-
-    var request = require('request');
-    var options = {
-      uri: 'https://casillerovirtual4-72.com.co/Registration/Tracking/gettrackingresult?trackinno='+parametro,
-      method: 'GET',
-      headers: {
-         'Content-Type': 'application/json'
-      }
-    };
-    console.log(options);
-    request(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body);
-        let jsonTracking=JSON.parse(body);
-	if(!isEmpty(jsonTracking)){
-        	let message="Hola *"+req.body.message.from.first_name+"*, su numero de guia *"+parametro+"* tiene estado *"+jsonTracking.dessta+"* enviado por *"+jsonTracking.rem_nombre+"*. Tubo un peso de *"+jsonTracking.pesolb+"* lbs, se recibio el dia *"+jsonTracking.recibo+"* y se recibio el pago el dia *"+jsonTracking.pagado+"*.";
-        	sendMessage(req,res,message);
-	}else{
-            sendMessage(req,res, "No se encontro informacion sobre la guia *"+parametro+"*, valide su guia.");
-	}
-      }else{
-        res.status(500).end();
-      }
-    });
-}else{
-res.status(200).end();
-}
-
-}else{
-res.status(200).end();
-}
-
-});*/
 
 var isEmpty = function(data) {
     if(typeof(data) === 'object'){
@@ -174,11 +65,6 @@ var isEmpty = function(data) {
         return false;
     }
 }
-
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
